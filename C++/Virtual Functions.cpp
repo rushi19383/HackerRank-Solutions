81895e54f8da427c951364ecdbdf76c5af0f5ad4
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2021-01-28 16:43:48 +0530

    Update Virtual Functions.cpp

--- C++/Virtual Functions.cpp
+++ C++/Virtual Functions.cpp
@@ -1,28 +1,84 @@
-static int i=0;
-static int j=0;
-
-class Person{
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class Person
+{
+    protected:
+      int age;
+      string name;
     public:
-        string name;
-        int age,score,id;
-        virtual void putdata()=0;
-        virtual void getdata()=0;
-        
+          virtual void getdata(){};
+          virtual void putdata(){};
+};
+class Professor : public Person
+{
+    int publication;
+    static int id1;
+      public:
+       void getdata()
+       {
+           cin>>name;
+           cin>>age;
+           cin>>publication;
+       }
+       void putdata()
+       {
+          cout<<name<<" "<<age<<" "<<publication<<" "<<id1<<endl;
+          id1++;
+       }
+};
+int Professor::id1=1;
+class Student : public Person
+{
+    int marks[6];
+     static int id2;
+  public:
+      int sum=0;
+   void getdata()
+   {
+       cin>>name;
+       cin>>age;
+       for(int i=0;i<=5;i++)
+       {
+          cin>>marks[i];
+          sum=sum+marks[i];
+       }
+   }
+   void putdata()
+   {
+      cout<<name<<" "<<age<<" "<<sum<<" "<<id2<<endl;
+      id2++;
+   }
 };
 
-class Professor : public Person{
-    void putdata(){
-        cout<<name<<" "<<age<<" "<<score<<" "<<++i<<endl;
+int Student::id2=1;
+
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
     }
-    void getdata(){
-        cin>>name>>age>>score;
-    }
-};
-class Student : public Person{
-    void putdata(){
-        cout<<name<<" "<<age<<" "<<score<<" "<<++j<<endl;
-    }
-    void getdata(){
-        cin>>name>>age;score=0;
-    }
-};
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
+}
