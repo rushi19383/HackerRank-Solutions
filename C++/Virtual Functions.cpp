010d05a396ef0bdc59a2bf47958b4f9e35aa89c3
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2021-01-28 16:41:46 +0530

    Update Virtual Functions.cpp

--- C++/Virtual Functions.cpp
+++ C++/Virtual Functions.cpp
@@ -1,51 +1,28 @@
-class Person
-{
-    protected:
-      int age;
-      string name;
+static int i=0;
+static int j=0;
+
+class Person{
     public:
-          virtual void getdata(){};
-          virtual void putdata(){};
-};
-class Professor : public Person
-{
-    int publication;
-    static int id1;
-      public:
-       void getdata()
-       {
-           cin>>name;
-           cin>>age;
-           cin>>publication;
-       }
-       void putdata()
-       {
-          cout<<name<<" "<<age<<" "<<publication<<" "<<id1<<endl;
-          id1++;
-       }
-};
-int Professor::id1=1;
-class Student : public Person
-{
-    int marks[6];
-     static int id2;
-  public:
-      int sum=0;
-   void getdata()
-   {
-       cin>>name;
-       cin>>age;
-       for(int i=0;i<=5;i++)
-       {
-          cin>>marks[i];
-          sum=sum+marks[i];
-       }
-   }
-   void putdata()
-   {
-      cout<<name<<" "<<age<<" "<<sum<<" "<<id2<<endl;
-      id2++;
-   }
+        string name;
+        int age,score,id;
+        virtual void putdata()=0;
+        virtual void getdata()=0;
+        
 };
 
-int Student::id2=1;
+class Professor : public Person{
+    void putdata(){
+        cout<<name<<" "<<age<<" "<<score<<" "<<++i<<endl;
+    }
+    void getdata(){
+        cin>>name>>age>>score;
+    }
+};
+class Student : public Person{
+    void putdata(){
+        cout<<name<<" "<<age<<" "<<score<<" "<<++j<<endl;
+    }
+    void getdata(){
+        cin>>name>>age;score=0;
+    }
+};
